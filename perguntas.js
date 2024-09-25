6cbe4c136e5bbe7588b1330cd8b558030a3cf566
Cicero1206p <160047998+Cicero1206p@users.noreply.github.com>
2024-09-25 14:01:44 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,62 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Internacional',
+    'Qual unico time ganhou o campeonato brasileiro invicto?',
+    'Internacional em 1979'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Internacional',
+    'Que jogador fez o gol que "pregou o caixão" do grêmio em 2020?',
+    'Taison. Gol de cabeça'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Grêmio',
+    'Quantas vezes o GreBio foi rebaixado?',
+    'Três vezes. A caminho da quarta.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Internacional',
+    'Quem o inter venceu para ser campeão mundial em 2006?',
+    'Barcelona. O qual jogava Ronaldinho Gaúcho'
+)
+
+criaCartao(
+    'Internacional' ,
+    'Quem marcou o gol do titulo mundial do inter em 2006?' ,
+    'Adriano "gabiru"'
+
+)
+
+criaCartao(
+    'Internacional' ,
+    'Quais jogadores fizeram os gols do título da Libertadores do inter em 2010?' ,
+    'Sóbis, Damião e Giuliano'
+)
+
+criaCartao(
+    'Internacional' ,
+    'Quem foi injustamente expulso no jogo que valia o título brasileiro de 2010?' ,
+    'Tinga' 
+)
+
+criaCartao(
+    'Internacional' , 
+    'Qual a contratação mais cara da história do inter?' ,
+    'Nico López por 35M de reais'
+
+)
+
+criaCartao(
+    'Internacional' ,
+    'Qual o pior técnico que já treinou o colorado?' ,
+    'Eduardo Coudet'
+
+)
+
+criaCartao(
+    'Internacional' ,
+    'Qual time tem mais títulos do campeonato gaúcho?' ,
+    'Internacional'
 )
